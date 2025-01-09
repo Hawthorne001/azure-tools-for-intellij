d13b46a7e240de12cffda63cb5dc07a398641539
Rival Abdrakhmanov <rival.abdrakhmanov@gmail.com>
2025-01-09 13:42:58 +0100

    Update run config producer

--- PluginsAndFeatures/azure-toolkit-for-rider/azure-intellij-plugin-appservice-dotnet/src/main/kotlin/com/microsoft/azure/toolkit/intellij/legacy/function/runner/localRun/FunctionRunConfigurationProducer.kt
+++ PluginsAndFeatures/azure-toolkit-for-rider/azure-intellij-plugin-appservice-dotnet/src/main/kotlin/com/microsoft/azure/toolkit/intellij/legacy/function/runner/localRun/FunctionRunConfigurationProducer.kt
@@ -12,12 +12,13 @@ import com.intellij.openapi.util.Ref
 import com.intellij.openapi.util.io.FileUtil
 import com.intellij.openapi.util.io.systemIndependentPath
 import com.intellij.psi.PsiElement
+import com.jetbrains.rd.util.firstOrNull
 import com.jetbrains.rider.model.runnableProjectsModel
 import com.jetbrains.rider.projectView.solution
 import com.jetbrains.rider.projectView.workspace.getFile
 import com.jetbrains.rider.run.configurations.getSelectedProject
+import com.jetbrains.rider.run.configurations.launchSettings.LaunchSettingsJsonService
 import com.microsoft.azure.toolkit.intellij.legacy.function.daemon.AzureRunnableProjectKinds
-import com.microsoft.azure.toolkit.intellij.legacy.function.launchProfiles.*
 import com.microsoft.azure.toolkit.intellij.legacy.function.launchProfiles.getApplicationUrl
 import com.microsoft.azure.toolkit.intellij.legacy.function.launchProfiles.getArguments
 import com.microsoft.azure.toolkit.intellij.legacy.function.launchProfiles.getEnvironmentVariables
@@ -33,14 +34,14 @@ class FunctionRunConfigurationProducer : LazyRunConfigurationProducer<FunctionRu
         configuration: FunctionRunConfiguration,
         context: ConfigurationContext
     ): Boolean {
-        val selectedProjectFilePathInvariant =
-            context.getSelectedProject()?.getFile()?.systemIndependentPath ?: return false
+        val selectedProjectFilePath = context.getSelectedProject()?.getFile()?.systemIndependentPath ?: return false
 
         val projects = context.project.solution.runnableProjectsModel.projects.valueOrNull ?: return false
+        val configurationProjectFilePath =  FileUtil.toSystemIndependentName(configuration.parameters.projectFilePath)
         val runnableProject = projects.firstOrNull {
             it.kind == AzureRunnableProjectKinds.AzureFunctions &&
-                    FileUtil.toSystemIndependentName(it.projectFilePath) == selectedProjectFilePathInvariant &&
-                    FileUtil.toSystemIndependentName(configuration.parameters.projectFilePath) == selectedProjectFilePathInvariant
+                    FileUtil.toSystemIndependentName(it.projectFilePath) == selectedProjectFilePath &&
+                    configurationProjectFilePath == selectedProjectFilePath
         }
 
         return runnableProject != null
@@ -51,41 +52,44 @@ class FunctionRunConfigurationProducer : LazyRunConfigurationProducer<FunctionRu
         context: ConfigurationContext,
         ref: Ref<PsiElement>
     ): Boolean {
-        val selectedProjectFilePathInvariant = context.getSelectedProject()?.getFile()?.systemIndependentPath
+        val selectedProjectFilePath = context.getSelectedProject()?.getFile()?.systemIndependentPath
             ?: return false
 
         val projects = context.project.solution.runnableProjectsModel.projects.valueOrNull ?: return false
         val runnableProject = projects.firstOrNull {
             it.kind == AzureRunnableProjectKinds.AzureFunctions &&
-                    FileUtil.toSystemIndependentName(it.projectFilePath) == selectedProjectFilePathInvariant
+                    FileUtil.toSystemIndependentName(it.projectFilePath) == selectedProjectFilePath
         } ?: return false
 
         if (configuration.name.isEmpty()) {
             configuration.name = runnableProject.name
         }
 
-        val projectOutput = runnableProject.projectOutputs.firstOrNull()
-        val launchProfile = FunctionLaunchProfilesService
-            .getInstance(context.project)
-            .getLaunchProfiles(runnableProject)
+        val projectOutput = runnableProject
+            .projectOutputs
             .firstOrNull()
+        val profile = LaunchSettingsJsonService
+            .getInstance(context.project)
+            .loadLaunchSettings(runnableProject)
+            ?.profiles
+            ?.firstOrNull()
 
         configuration.parameters.apply {
-            projectFilePath = selectedProjectFilePathInvariant
+            projectFilePath = selectedProjectFilePath
             projectTfm = projectOutput?.tfm?.presentableName ?: ""
-            profileName = launchProfile?.name ?: ""
+            profileName = profile?.key ?: ""
             functionNames = ""
             trackArguments = true
-            arguments = getArguments(launchProfile?.content, projectOutput)
+            arguments = getArguments(profile?.value, projectOutput)
             trackWorkingDirectory = true
-            workingDirectory = getWorkingDirectory(launchProfile?.content, projectOutput)
+            workingDirectory = getWorkingDirectory(profile?.value, projectOutput)
             trackEnvs = true
-            envs = getEnvironmentVariables(launchProfile?.content)
+            envs = getEnvironmentVariables(profile?.value)
             useExternalConsole = false
             trackUrl = true
             startBrowserParameters.apply {
-                url = getApplicationUrl(launchProfile?.content, projectOutput, null)
-                startAfterLaunch = launchProfile?.content?.launchBrowser == true
+                url = getApplicationUrl(profile?.value, projectOutput, null)
+                startAfterLaunch = profile?.value?.launchBrowser == true
             }
         }
 
