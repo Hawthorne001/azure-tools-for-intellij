16e295f61fd61a27324e0aae9c337c2ddf450beb
Ilnar Agliamzianov <ilnar.agliamzianov@jetbrains.com>
2025-01-14 14:13:39 +0200

    Pass scope to RemoteMachine.guess

--- PluginsAndFeatures/azure-toolkit-for-rider/azure-intellij-plugin-appservice-dotnet/src/main/kotlin/com/microsoft/azure/toolkit/intellij/debugger/AzureRemoteDebuggerTools.kt
+++ PluginsAndFeatures/azure-toolkit-for-rider/azure-intellij-plugin-appservice-dotnet/src/main/kotlin/com/microsoft/azure/toolkit/intellij/debugger/AzureRemoteDebuggerTools.kt
@@ -5,17 +5,20 @@
 package com.microsoft.azure.toolkit.intellij.debugger
 
 import com.intellij.openapi.project.Project
+import com.intellij.platform.util.coroutines.childScope
 import com.intellij.remote.RemoteCredentials
 import com.jetbrains.rider.PathInfo
 import com.jetbrains.rider.debugger.attach.remoting.machines.RemoteMachine
 import com.jetbrains.rider.debugger.attach.remoting.tools.remote.RemoteDebuggerToolsBase
+import com.microsoft.azure.toolkit.intellij.AppServiceProjectService
 
 class AzureRemoteDebuggerTools(remoteMachine: RemoteMachine, project: Project) :
     RemoteDebuggerToolsBase(remoteMachine, project) {
 
     companion object {
         suspend fun create(credentials: RemoteCredentials, project: Project): AzureRemoteDebuggerTools {
-            val remoteMachine = RemoteMachine.guess(credentials)
+            val scope = AppServiceProjectService.getInstance(project).scope.childScope("RemoteMachineLazy")
+            val remoteMachine = RemoteMachine.guess(scope, credentials)
             return AzureRemoteDebuggerTools(remoteMachine, project)
         }
     }
