03bea02bfb949b6fe8b0b8cd11e61f21bd70f24f
Ilnar Agliamzianov <ilnar.agliamzianov@jetbrains.com>
2025-01-21 15:57:16 +0200

    Fix AzureAppServicePingingService

--- PluginsAndFeatures/azure-toolkit-for-rider/azure-intellij-plugin-appservice-dotnet/src/main/kotlin/com/microsoft/azure/toolkit/intellij/debugger/pings/AzureAppServicePingingService.kt
+++ PluginsAndFeatures/azure-toolkit-for-rider/azure-intellij-plugin-appservice-dotnet/src/main/kotlin/com/microsoft/azure/toolkit/intellij/debugger/pings/AzureAppServicePingingService.kt
@@ -6,47 +6,42 @@ package com.microsoft.azure.toolkit.intellij.debugger.pings
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.rd.createNestedDisposable
 import com.intellij.xdebugger.XDebugProcess
-import com.intellij.xdebugger.XDebuggerManager
-import com.intellij.xdebugger.XDebuggerManagerListener
 import com.intellij.xdebugger.impl.XDebugSessionImpl
-import com.jetbrains.rd.platform.util.idea.LifetimedService
 import com.jetbrains.rd.util.concurrentMapOf
 import com.jetbrains.rider.run.AttachSshDebugProfileStateBase
+import com.jetbrains.rider.util.idea.getService
 import com.microsoft.azure.toolkit.intellij.debugger.attachHosts.AppServiceAttachHost
 import com.microsoft.azure.toolkit.lib.appservice.function.FunctionApp
 import com.microsoft.azure.toolkit.lib.appservice.function.FunctionAppBase
 import kotlinx.coroutines.CoroutineScope
 
 @Service(Service.Level.PROJECT)
-class AzureAppServicePingingService(project: Project, private val cs: CoroutineScope) : LifetimedService() {
+class AzureAppServicePingingService(private val cs: CoroutineScope) {
     private val map = concurrentMapOf<XDebugProcess, FunctionPinger>()
 
-    init {
-        val disposable = serviceLifetime.createNestedDisposable()
-        project.messageBus.connect(disposable).subscribe(XDebuggerManager.TOPIC, DebugListener())
+    companion object {
+        fun getInstance(project: Project) = project.getService<AzureAppServicePingingService>()
     }
 
-    private inner class DebugListener : XDebuggerManagerListener {
-        override fun processStarted(debugProcess: XDebugProcess) {
-            val functionApp = getFunctionApp(debugProcess) ?: return
+    fun debuggingStarted(debugProcess: XDebugProcess) {
+        val functionApp = getFunctionApp(debugProcess) ?: return
 
-            map.computeIfAbsent(debugProcess) {
-                FunctionPinger(functionApp, cs).apply { start() }
-            }
+        map.computeIfAbsent(debugProcess) {
+            FunctionPinger(functionApp, cs).apply { start() }
         }
+    }
 
-        override fun processStopped(debugProcess: XDebugProcess) {
-            map.remove(debugProcess)?.stop()
-        }
+    fun debuggingStopped(debugProcess: XDebugProcess) {
+        map.remove(debugProcess)?.stop()
+    }
 
-        private fun getFunctionApp(debugProcess: XDebugProcess): FunctionAppBase<*, *, *>? {
-            val state = (debugProcess.session as? XDebugSessionImpl)
-                ?.executionEnvironment
-                ?.state as? AttachSshDebugProfileStateBase ?: return null
+    private fun getFunctionApp(debugProcess: XDebugProcess): FunctionAppBase<*, *, *>? {
+        val state = (debugProcess.session as? XDebugSessionImpl)
+            ?.executionEnvironment
+            ?.state as? AttachSshDebugProfileStateBase ?: return null
 
-            return (state.attachHost as? AppServiceAttachHost<FunctionApp>)?.appServiceApp
-        }
+        return (state.attachHost as? AppServiceAttachHost<FunctionApp>)?.appServiceApp
     }
 }
+
